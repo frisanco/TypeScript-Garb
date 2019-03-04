fad8fd414d73a2e8861a93260d7b89993e19bd10
Benjamin Frisanco <frisanco@cse.msu.edu>
2019-03-03 23:37:48 -0500

    Edit POST, remove HTML

--- greeter-post.php
+++ greeter-post.php
@@ -5,10 +5,10 @@ echo '<pre>';
 print_r($_POST); 
 echo '</pre>';
 
-$superval = strip_tags($_REQUEST['superhero']);
-$creamval = strip_tags($_REQUEST['icecream']);
-$tvval = strip_tags($_REQUEST['tv']);
-$sportval = strip_tags($_REQUEST['sport']);
+$superval = strip_tags($_POST['superhero']);
+$creamval = strip_tags($_POST['icecream']);
+$tvval = strip_tags($_POST['tv']);
+$sportval = strip_tags($_POST['sport']);
 
 header("location:greeter.php");
 exit;
